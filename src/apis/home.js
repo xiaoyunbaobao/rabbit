e20cfb6e49540872be418f083996219155eed836
guoyiwei20051201 <1147619151@qq.com>
2026-04-20 21:43:33 +0800

    Home新鲜好物

--- src/apis/home.js
+++ src/apis/home.js
@@ -6,4 +6,15 @@ export function getBannerAPI() {
   return httpInstance({
     url: '/home/banner'
   })
-} 
+}
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+  return httpInstance({
+    url: '/home/new'
+  })
+}
