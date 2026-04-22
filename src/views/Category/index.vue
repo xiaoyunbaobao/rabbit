176e44f53e06b004a9d9e6850b22716fbad64254
guoyiwei20051201 <1147619151@qq.com>
2026-04-22 16:11:12 +0800

    一级分类 轮播图功能实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,6 +2,7 @@
 import {getCategoryAPI} from '@/apis/Category'
 import { onMounted, ref } from 'vue'
 import {useRoute} from 'vue-router'
+import { getBannerAPI } from '@/apis/home';
 
 // 获取数据
 const categoryData = ref({})
@@ -12,6 +13,20 @@ const getCategory = async() => {
 }
 onMounted(() =>
   getCategory()
+)
+
+
+// 获取Banner
+const bannerList = ref([]);
+const getBanner = async () => {
+  const res = await getBannerAPI({
+    distributionSite: '2'
+  })
+  // console.log(res);
+  bannerList.value = res.result;
+}
+onMounted(() =>
+  getBanner()
 )
 </script>
 
@@ -25,6 +40,14 @@ onMounted(() =>
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- 轮播图 -->
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
     </div>
   </div>
 </template>
@@ -106,6 +129,15 @@ onMounted(() =>
 
   .bread-container {
     padding: 25px 0;
+  }
+}
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+  img {
+    width: 100%;
+    height: 500px;
   }
 }
 </style>
