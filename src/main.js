4a15fef9217275c6bf96d6c19bbac7ccdcd0c3ee
guoyiwei20051201 <1147619151@qq.com>
2026-04-22 12:55:57 +0800

    图片懒加载实现初步

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import router from './router'
 
 // 测试引入初始化样式文件
 import '@/styles/common.scss'
+// 导入vueUse里的检测图片是否进入视图的函数
+import { useIntersectionObserver } from '@vueuse/core'
 
 
 // // 测试接口函数
@@ -20,3 +22,23 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+// directive是一个对象 用于注册对当前组件实例可用的指令
+app.directive('img-lazy', {
+  // 指令绑定的元素el
+  // binding:binding.value 指令=后面绑定的表达式的值 图片url绑定在binding.value上
+  mounted(el, binding) {
+    console.log(el, binding.value);
+    // useIntersectionObserver是vueUse提供的一个函数 用于检测元素是否进入视口区域
+    useIntersectionObserver(
+      el,
+      // isIntersecting相当于boolean值 进入视口区域为true 没有进入为false
+      ([{ isIntersecting }]) => {
+        // console.log(isIntersecting);
+        if (isIntersecting) {
+          // 进入视口区域
+          el.src = binding.value
+        }
+      },
+    )
+  }
+})
