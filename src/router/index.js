da6c157868e805c0f3ede74b8020d8e62c7b04c6
guoyiwei20051201 <1147619151@qq.com>
2026-04-22 14:38:45 +0800

    首页导航栏(包括超过视口展开的fix导航栏)一级分类点击分类标签跳转激活

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,7 @@ const router = createRouter({
           component: Home
         },
         {
-          path: 'category',
+          path: 'category/:id',
           component: Category
         }
       ]
